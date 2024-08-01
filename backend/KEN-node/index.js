52c677395c1008308bedbb924fbde8a601363ce8
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2024-07-31 22:41:04 -0700

    some changes

--- backend/KEN-node/index.js
+++ backend/KEN-node/index.js
@@ -25,8 +25,24 @@ const cors = require('cors');
 
 var nodemailer = require('nodemailer');
 
+// Create transport from .env account info
+const KenMailer = nodemailer.createTransport({
+    host: "mail.ihmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PWD
+    },
+    tls: {
+        ciphers: 'HIGH:MEDIUM:!aNULL:!eNULL:@STRENGTH:!DH:!kEDH'
+    }
+});
+
+// MAKE SURE EXPRESS APP USES BOTH CORS AND ALLOWS FOR JSON DATA TRANSMISSION
 app.use(cors()).use(express.json());
 
 app.listen(process.env.KEN_SERVER_PORT, async () => {
     console.log("KEN IS UP running on port " + process.env.KEN_SERVER_PORT);
 });
+
